5b470d4dbb0d8e6dbf449541e38b6bc854374970
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-03-03 22:47:03 +0300

    Initial Commit

---
+++ AirQualityMap/AirQualityMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityMapApp.swift
+//  AirQualityMap
+//
+//  Created by Vefa Kosova on 3.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
